958a75a2bdd49ebcc46f9cdc989c18aad4898f78
Jonathan Hulme <jon@digipigeon.com>
2020-06-25 18:28:12 +0000

    Add Rest Documentation

---
+++ stubs/rest.js
@@ -0,0 +1,82 @@
+ /**
+ * ConnexCS API
+ * @module cxRest
+ * @namespace cxRest
+ * @class
+ * @hideconstructor
+ * @classdesc Communicate back into ConnexCS through the  (https://api-docs.connexcs.com/)[API]
+ * 
+ * ## Example ##
+ * ```
+ * const cxRest = require('cxRest');
+ * const rest = cxRest.auth("api@yourdomain.com"); 
+ * 
+ * async function main(data) {
+ *   var result = await rest.read(data.routing.dest_number);
+ *   // Do something with result
+ *   return data
+ * }
+ * ```
+ */
+
+function rest () {
+	
+	/**
+	 * Create Record
+	 * 
+	 * @function create
+	 * @memberof cxRest
+	 * @param {string} area - Datastore Name
+	 * @param {string|number} key - Row Id
+	 * @param {object} data - Data Object / Row
+	 * @return {Promise}
+	 *
+	 * @example
+	 * await rest.create('customer', {name: 'Joe Blogs'}})
+	 */
+	async function create(area, key, data) { }
+
+	/**
+	 * Create Record
+	 * 
+	 * @function read
+	 * @memberof cxRest
+	 * @param {string} read - Datastore Name
+	 * @param {string|number} key - Row Id
+	 * @return {Promise<object>}
+	 *
+	 * @example
+	 * var data = await rest.read('customer/1234')
+	 */
+	async function read (area, key) { }
+
+	/**
+	 * Update Record
+	 * 
+	 * @function update
+	 * @memberof cxRest
+	 * @param {string} area - Datastore Name
+	 * @param {string|number} key - Row Id
+	 * @param {object} data - Data Object / Row
+	 * @return {Promise}
+	 *
+	 * @example
+	 * await rest.update('customer/1234', {name: 'Alice'})
+	 */
+	async function update(area, key, data) { }
+
+	/**
+	 * Delete Record
+	 * 
+	 * @function delete
+	 * @memberof cxRest
+	 * @param {string} area - Datastore Name
+	 * @param {string|number} key - Row Id
+	 * @return {Promise}
+	 *
+	 * @example
+	 * await rest.delete('customer/1234')
+	 */
+	async function delete (area, key) {	}
+	return self;
+}
