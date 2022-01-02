d8bea126cc7a90a9bbe9e9e3ed5787c69762c3da
Jonathan Hulme <jhulme@connexcs.com>
2022-01-02 14:58:06 +0000

    Add Job and Originate

---
+++ stubs/originate.js
@@ -0,0 +1,30 @@
+/**
+ * Call Origination
+ * @module cxOriginate
+ * @namespace cxOriginate
+ * @class
+ * @hideconstructor
+ * @classdesc Originate outbound calls from ConnexCS
+ * 
+ */
+
+function originate (){
+	/**
+	 * Originate
+	 * 
+	 * @function originate
+	 * @memberof cxOriginate
+	 * @param {number} params - companyId
+	 * @param {string} params - serverAlias
+	 * @param {string} params - destination
+	 * @param {string} params - cli
+	 * @param {string} params - extension
+	 * @return {Promise}
+	 *
+	 * @example
+	 * await kv.get(1234, 'myServerName', '123456789', '1122334455', '160');
+	 */
+	async function originate (companyId, alias, destination, cli, extension){
+
+	}
+}
